6f9fcacaec7c056ab096f14e9b39325909af83ba
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-05-17 13:25:06 +0200

    Corregidos (mejorados) los ejercicios anteriores

--- ejercicioParametros.sh
+++ ejercicioParametros.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+#Ejercicio 1
+
+#El valor de las variables es asignado al escribir en la consola durante la ejecución de este script
 echo "Los parametros en la posición 1 y 3 son: '$1' y '$3'"
+
+
+sleep 1
+
+
+#Ejercicio 2
+
+#$0 determina el fichero actual y $# cuenta los parametros que has introducido durante la ejecución
 echo "En el fichero $0 existen $# parametros"
--- ejercicioVariables.sh
+++ ejercicioVariables.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+#Asignados manualmente valores a las variables
+
 nombre="Miguel Angel"
 edad="31"
 ciudad="Sevilla"
 
+
 echo "Mi nombre es $nombre, tengo $edad años, vivo en $ciudad"
 
+
+#Ejercicio 2
+
+#read asigna el input introducido como valor a la variable que se le asigna
 echo ¿Cual es tu comida favorita?
 read comida
 echo ¿Cual es tu actividad favorita?
