f85481f1f5bfd07f0fee18b7d785f0d751dd8532
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-05-17 09:54:23 +0200

    all gone

--- ejercicioVariables1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-nombre="Miguel Angel"
-edad="31"
-ciudad="Sevilla"
-
-echo "Mi nombre es $nombre, tengo $edad años, vivo en $ciudad"
--- ejercicioVariables2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo ¿Cual es tu comida favorita?
-read comida
-echo ¿Cual es tu actividad favorita?
-read actividad
-
-echo “Mi actividad favorita es $actividad. Mi comida favorita es $comida”
