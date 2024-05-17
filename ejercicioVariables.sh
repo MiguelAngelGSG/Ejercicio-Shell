edae9875867b3d6b5d84bc12d271be7995c5e6e6
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-05-17 09:59:53 +0200

    Re-subida Variables

---
+++ ejercicioVariables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+nombre="Miguel Angel"
+edad="31"
+ciudad="Sevilla"
+
+echo "Mi nombre es $nombre, tengo $edad años, vivo en $ciudad"
+
+echo ¿Cual es tu comida favorita?
+read comida
+echo ¿Cual es tu actividad favorita?
+read actividad
+
+echo “Mi actividad favorita es $actividad. Mi comida favorita es $comida”
