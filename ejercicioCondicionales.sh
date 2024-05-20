e487cd924ab6ab12872339fe420283cefe385879
MiguelAngelGSG <ma.gsg.555@gmail.com>
2024-05-20 08:23:36 +0200

    correcion Condicionales

--- ejercicioCondicionales.sh
+++ ejercicioCondicionales.sh
@@ -2,15 +2,15 @@
 
 #Ejercicio 1
 
-# Obtain current user
+#Obtener usuario actual
 current_user=$(whoami)
 
 
-# Check if the current user can run commands as root without a password prompt
-# 2>/dev/null suppresses error messages
-# sudo commands are only usable by the super user
-# -n asks to not be prompt a password
-# this code will try to have access to sudo without using a password, if it doesn't return true you don't have root access
+#Comprueba si el usuario actual puede utilizar comandos como root sin contraseña
+# 2>/dev/null elimina posibles mensajes de error
+#Los comandos 'sudo' solo pueden ser utilizados por el 'super user'
+#-n dice a la maquina que no nos pida contraseña
+#Este codigo trata de usar 'sudo' sin contraseña, si no devuelve 'true' no tienes acceso root
 if sudo -n true 2>/dev/null; then
     echo "El usuario '$current_user' tiene acceso a root."
 else
@@ -40,8 +40,8 @@ if ! [[ $secondNumber =~ ^[0-9]+$ ]]; then
     exit
 fi
 
-# -eq compara ambas Variables para ver si son iguales
-# gt (greater than) compara si la primera Variable es mayor que la segunda
+#-eq compara ambas Variables para ver si son iguales
+#gt (greater than) compara si la primera Variable es mayor que la segunda
 
 if [ $firstNumber -eq $secondNumber ]; then
     echo "Los números son iguales."
